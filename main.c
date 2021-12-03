c01b0db0a8ff18a5a248aef29d2e895cbe580a1d
Mohammad Olyaiy <mohammad@Mohammads-MacBook-Air.local>
2021-12-03 12:24:41 -0800

    first commit

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "vbx.h"
+
+
+vbxsim_init(
+    4,      // numb_lanes
+    64,     //scratchpad capacity
+    256,    //max_masked_waves
+    6,      //fxp_word_frac_bits
+    5,      //fxp_half_frac_bits
+    4,      //fxp_byte_frac_bits
+);
+
+
+void mm(float *A, float *B, float *Res)
+{
+    //create vector scratchpads with the size of matrix for both
+    //operands and the result
+	  vbx_word_t *a = vbx_sp_malloc(N * N * sizeof(vbx_word_t));
+	  vbx_word_t *b = vbx_sp_malloc(N * N * sizeof(vbx_word_t));
+	  vbx_word_t *res = vbx_sp_malloc(N * N * sizeof(vbx_word_t));
+    //transfering data from matrix arrays to vector scratchpads
+    //scratchpad ptr, host ptr, numBytes
+    vbx_dma_to_vector(a, A, N * N * sizeof(vbx_word_t));
+    vbx_dma_to_vector(b, B, N * N * sizeof(vbx_word_t));
+    vbx_sync();
+    //setting vector length register
+    //num lanes=N, num_rows=N
+    vbx_set_vl(N, N);
+    //stride -> num_rows, incDest, incSrcA
+    vbx_set_2D(N, N, N);
+    //vector-vector, words size, unsigned, dest->c, sources->a, b
+    vbx(VVWWWUUU, VMUL, c, a, b);
+  	vbx_sync();
+    vbx_dma_to_host(C, c, N * N * sizeof(vbx_word_t));
+    vbx_sync();
+    vbxsim_print_stats();
+    vbx_sp_free();
+	  return;
+}
+
+
+
+int main(){
+
+  float *A = create_matrix();
+  float *B = create_matrix();
+  float *C = create_matrix();
+  //randomly initialize A and B
+  initialize_matrix(A, 1, 0);
+  initialize_matrix(B, 1, 0);
+  print_matrix(A);
+  print_matrix(B);
+
+  return 0;
+}
---
+++ matr.h
@@ -0,0 +1,63 @@
+#ifndef MATR_H
+#define MATR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+#if !defined(N)
+#define N 4096
+#endif
+
+#if !defined(PARALLEL)
+#define PARALLEL 0
+#endif
+
+
+
+float *create_matrix(){
+  float *matrix = (float *)malloc(sizeof(float) * N * N);
+  return matrix;
+}
+
+
+void initialize_matrix(float *data, int random_init, float init_val){
+  float max_random_init = 1000.0;
+  int max_random_init_int = 1000;
+
+  for(int i=0 ; i<N ; i++){
+    for(int j=0 ; j<N ; j++){
+      if(random_init)
+        data[i * N + j] = (float)rand() / (float)(RAND_MAX/max_random_init) + 1;
+      else
+        data[i * N + j] = init_val;
+    }
+  }
+}
+
+
+void initialize_matrix_from_another_matrix(float *dest, float *source){
+  for(int i=0 ; i<N ; i++){
+    for(int j=0 ; j<N ; j++){
+      dest[i * N + j] = source[i * N + j];
+    }
+  }
+}
+
+
+void print_matrix(float *data){
+  for (int i=0 ; i<N ; i++){
+    for(int j=0 ; j<N ; j++)
+      printf("%.2f  ", (data)[i*N+j]);
+    printf("\n");
+  }
+}
+
+
+void free_matrix(float *data){
+  free(data);
+}
+
+#endif
