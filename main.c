ee5fb0a2d7c1eb510ad5946e64f5c2f69782bb2c
Mohammad Olyaiy <mohammad@Mohammads-MacBook-Air.local>
2021-12-03 12:51:21 -0800

    debug

--- main.c
+++ main.c
@@ -5,33 +5,32 @@
 
 
 vbxsim_init(
-    4,      // numb_lanes
-    64,     //scratchpad capacity
-    256,    //max_masked_waves
-    6,      //fxp_word_frac_bits
-    5,      //fxp_half_frac_bits
-    4,      //fxp_byte_frac_bits
+    4,      /* numb_lanes*/
+    64,     /*scratchpad capacity*/
+    256,    /*max_masked_waves*/
+    6,      /*fxp_word_frac_bits*/
+    4,      /*fxp_byte_frac_bits*/
 );
 
 
 void mm(float *A, float *B, float *C)
 {
-    //create vector scratchpads with the size of matrix for both
-    //operands and the result
+    /*create vector scratchpads with the size of matrix for both*/
+    /*operands and the result*/
 	  vbx_word_t *a = vbx_sp_malloc(N * N * sizeof(vbx_word_t));
 	  vbx_word_t *b = vbx_sp_malloc(N * N * sizeof(vbx_word_t));
 	  vbx_word_t *c = vbx_sp_malloc(N * N * sizeof(vbx_word_t));
-    //transfering data from matrix arrays to vector scratchpads
-    //scratchpad ptr, host ptr, numBytes
+    /*transfering data from matrix arrays to vector scratchpads*/
+    /*scratchpad ptr, host ptr, numBytes*/
     vbx_dma_to_vector(a, A, N * N * sizeof(vbx_word_t));
     vbx_dma_to_vector(b, B, N * N * sizeof(vbx_word_t));
     vbx_sync();
-    //setting vector length register
-    //num lanes=N, num_rows=N
+    /*setting vector length register*/
+    /*num lanes=N, num_rows=N*/
     vbx_set_vl(N, N);
-    //stride -> num_rows, incDest, incSrcA
+    /*stride -> num_rows, incDest, incSrcA*/
     vbx_set_2D(N, N, N);
-    //vector-vector, words size, unsigned, dest->c, sources->a, b
+    /*vector-vector, words size, unsigned, dest->c, sources->a, b*/
     vbx_acc(VVWWWUUU, VMUL, c, a, b);
   	vbx_sync();
     vbx_dma_to_host(C, c, N * N * sizeof(vbx_word_t));
@@ -48,7 +47,7 @@ int main(){
   float *A = create_matrix();
   float *B = create_matrix();
   float *C = create_matrix();
-  //randomly initialize A and B
+  /*randomly initialize A and B*/
   initialize_matrix(A, 1, 0);
   initialize_matrix(B, 1, 0);
   print_matrix(A);
